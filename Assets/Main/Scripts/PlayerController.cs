81e5d26912375e81b07ca026896c149d4ebfd9dd
SatoShinta <kanten3005@gmail.com>
2024-12-24 20:42:14 +0900

    仮のplayer移動システムを完成させた。次は連打でスピードアップさせる処理を書きたい

--- Assets/Main/Scripts/PlayerController.cs
+++ Assets/Main/Scripts/PlayerController.cs
@@ -1,16 +1,42 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField, Header("playerのリジットボディ")] Rigidbody _playerRB;
+    [SerializeField, Header("playerの移動速度")] float _moveSpeed = 1.0f;
+
+    InputSystem_Actions _inputActions;
+    Vector2 _moveInputValue;
+
     void Start()
     {
-        
+        _inputActions = new InputSystem_Actions();
+
+        _inputActions.Player.Move.started += OnMove;
+        _inputActions.Player.Move.performed += OnMove;
+        _inputActions.Player.Move.canceled += OnMove;
+
+        _inputActions.Enable(); // 忘れずに…
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 入力をVector2に変換して受け取るメソッド
+    /// </summary>
+    /// <param name="context"></param>
+    private void OnMove(InputAction.CallbackContext context)
     {
-        
+        _moveInputValue = context.ReadValue<Vector2>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        _playerRB.AddForce(new Vector3(_moveInputValue.x, 0f, _moveInputValue.y) * _moveSpeed, ForceMode.Force);
+    }
+
+    private void OnDestroy()
+    {
+        _inputActions.Dispose(); // 忘れずに…
     }
 }
